8f4d2dda991c9aab459ca1c0bd97eb6418889a83
umerishaq123 <umerishaq7387@gmail.com>
2025-05-21 18:51:21 +0500

    final code new,check now umer es ma be hereabc nnnn

--- middlewares/not_found.js
+++ middlewares/not_found.js
@@ -1,4 +1,8 @@
-// const notFound = (req, res, next) => {
-//   res.status(404).json({ success: false, message: "Path not found" });
-// };
-// module.exports = notFound;
+const notFound = (req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: `Path not found: ${req.originalUrl}`
+  });
+};
+
+module.exports = notFound;
