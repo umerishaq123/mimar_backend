ae8eb7d1427dc7223e450acb9f917d858647893e
umerishaq123 <umerishaq7387@gmail.com>
2025-05-21 12:26:04 +0500

    Add start script for Railwayhhhhhjjjjjkkkkumer

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login } = require('../conttrollers/auth_Controller');
+const { signup, login } = require('../conttrollers/auth_controller');
 
 router.post('/signup', signup);
 router.post('/login', login);
